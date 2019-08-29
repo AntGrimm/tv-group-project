55bf2fe06e34474fbf0531fa436d887c87409944
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-29 16:15:58 -0400

    some other changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import Homepage from './pages/Homepage'
+import TvShow from './pages/TvShow'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return <Homepage />
+    return (
+      <>
+        <header>
+          <h1>TV Shows!</h1>
+        </header>
+        <main>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Homepage}></Route>
+              <Route exact path="/tvShow" component={TvShow}></Route>
+            </Switch>
+          </Router>
+        </main>
+      </>
+    )
   }
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Homepage = () => {
   return (
     <>
-      <header>
-        <h1>Top Rated TV Shows</h1>
-      </header>
       <ul>
         <header>Top Rated TV Show of the Day!</header>
         <li>TV Show</li>
