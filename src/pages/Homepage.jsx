cd128f114484f041e39040e06f2bc319c68bfacd
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-29 13:51:23 -0400

    Made initial wirefram

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+      <header>
+        <h1>Top Rated TV Shows</h1>
+      </header>
+      <ul>
+        <header>Top Rated TV Show of the Day!</header>
+        <li>TV Show</li>
+      </ul>
+      <ul>
+        <li>TV Show 1</li>
+        <li>TV Show 2</li>
+        <li>TV Show 3</li>
+        <li>TV Show 4</li>
+      </ul>
+    </>
+  )
+}
+
+export default Homepage
