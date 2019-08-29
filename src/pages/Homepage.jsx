f133a1b26733d0cde933d0947b628c931e15d32c
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-29 17:09:58 -0400

    lowercase a

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Axios from 'Axios'
+import axios from 'axios'
 
 const Homepage = () => {
   const fetchDataShows = async () => {
-    const resp = await Axios.get(
+    const resp = await axios.get(
       'https://api.themoviedb.org/3/tv/top_rated?api_key=777aea70df4e4c6df6c5d0195ce2d746&language=en-US&page=1'
     )
     console.log(resp.data)
