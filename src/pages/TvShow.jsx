dddcc98210e0e534e32cccee55c8dd7b7b198f37
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-30 21:08:47 -0400

    added a back button

--- src/pages/TvShow.jsx
+++ src/pages/TvShow.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const TvShow = () => {
   // const tvSearchTerm = props.params.match.results
@@ -30,6 +31,7 @@ const TvShow = () => {
 
   return (
     <>
+      <Link to="/"> &laquo; Back </Link>
       <section>
         <ul>
           {showData.map((show, i) => {
