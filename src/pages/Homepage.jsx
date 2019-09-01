e10b5336ef6a95b2b555704bb2e0b26e61ae96d4
Michelle Polinsky <michelle.polinsky@gmail.com>
2019-09-01 15:46:25 -0400

    displays show info

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,29 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Homepage = () => {
+  const [displayShows, setDisplayShows] = useState([])
+
   const fetchDataShows = async () => {
     const resp = await axios.get(
       'https://api.themoviedb.org/3/tv/top_rated?api_key=777aea70df4e4c6df6c5d0195ce2d746&language=en-US&page=1'
     )
+
     console.log(resp.data)
+    setDisplayShows(resp.data.results)
   }
+
+  useEffect(() => {
+    fetchDataShows()
+  }, [])
   return (
     <>
-      <ul>
-        <header>Top Rated TV Show of the Day!</header>
-        <li>TV Show</li>
-      </ul>
-      <ul>
-        <li>TV Show 4</li>
-        <li>TV Show 2</li>
-        <li>TV Show 3</li>
-        <li>TV Show 4</li>
-        <li>TV Show 7</li>
-        <li>TV Show 7</li>
-      </ul>
+      <header>Top Rated TV Show of the Day!</header>
+      <h2>Random tv show here</h2>
+      {displayShows.map((results, i) => {
+        return (
+          <div key={i}>
+            <div className="image-cont">
+              <img
+                className="col s3"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${results.poster_path}`}
+                alt=""
+              />
+            </div>
+
+            <p>{results.name}</p>
+            <p>{results.first_air_date}</p>
+            <p>{results.overview}</p>
+            <p>Vote Average {results.vote_average}</p>
+          </div>
+        )
+      })}
     </>
   )
 }
 
 export default Homepage
+
+{
+  /* <section className="show-list">
+          <div className="row">
+            {tvShows.map((tvShow, i) => {
+              return (
+                <div key={i} className="col l6 show-card-area">
+                  <div className="show-poster-area">
+                    <Link to={`/tv/${tvShow.id}`}>
+                      <img
+                        className="col s3"
+                        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${tvShow.poster_path}`}
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <section className="col s6">
+                    <h3>{tvShow.name}</h3>
+                    <p>Rating: {tvShow.vote_average}</p>
+                    <p>First Aired: {tvShow.first_air_date}</p>
+                    <p>{tvShow.overview}</p>
+                  </section> */
+}
