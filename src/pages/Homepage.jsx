a4e563e80adca8f1c6f26ccff92ad4b6767c2ae3
Michelle Polinsky <michelle.polinsky@gmail.com>
2019-09-02 20:00:45 -0400

    added random but broken

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,6 +6,8 @@ import TvShow from './TvShow.jsx'
 const Homepage = () => {
   const [displayShows, setDisplayShows] = useState([])
 
+  const [randomShow, setRandomShow] = useState(0)
+
   const fetchDataShows = async () => {
     const resp = await axios.get(
       'https://api.themoviedb.org/3/tv/top_rated?api_key=777aea70df4e4c6df6c5d0195ce2d746&language=en-US&page=1'
@@ -13,6 +15,7 @@ const Homepage = () => {
 
     console.log(resp.data)
     setDisplayShows(resp.data.results)
+    setRandomShow(Math.floor(Math.random() * resp.data.results.length))
   }
 
   useEffect(() => {
@@ -21,29 +24,33 @@ const Homepage = () => {
   return (
     <>
       <h1 className="header">Top Rated TV Show of the Day!</h1>
-      <h2 className="rando-cont">Random tv show here</h2>
-      {displayShows.map((results, i) => {
-        return (
-          <div key={i}>
-            <section className="body-cont">
-              <div className="image-cont">
-                <img
-                  className="poster"
-                  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${results.poster_path}`}
-                  alt=""
-                />
-              </div>
-              <div className="show-info-cont">
-                <h3 className="title">{results.name}</h3>
-                <p>First Aired {results.first_air_date}</p>
-                <p>{results.overview}</p>
-                <p>Vote Average {results.vote_average}</p>
-                <Link to="/TvShow">Cast</Link>
-              </div>
-            </section>
-          </div>
-        )
-      })}
+      {/* <h2 className="rando-cont">{displayShows[randomShow].name}</h2> */}
+      <main>
+        {displayShows.map((results, i) => {
+          return (
+            <div key={i}>
+              <section className="body-cont">
+                <div className="image-cont">
+                  <img
+                    className="poster"
+                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${results.poster_path}`}
+                    alt=""
+                  />
+                </div>
+                <div className="show-info-cont">
+                  <h3 className="title">{results.name}</h3>
+                  <p>First Aired {results.first_air_date}</p>
+                  <p>{results.overview}</p>
+                  <p>Vote Average {results.vote_average}</p>
+                  <Link to="/TvShow" className="cast-link">
+                    Cast
+                  </Link>
+                </div>
+              </section>
+            </div>
+          )
+        })}
+      </main>
     </>
   )
 }
